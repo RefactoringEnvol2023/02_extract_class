ef90b19a91cb46437da1386ef21503fcaef4ae4a
Marc DeXeT <marc.dexet@ias.u-psud.fr>
2023-11-15 00:40:42 +0100

    Step1

--- src/reporting/models.py
+++ src/reporting/models.py
@@ -3,22 +3,27 @@ import sys
 from typing import Optional
 
 
-class StudentReport:
+class Report:
+
+    def generate_report(self, student, out: Optional[StringIO]=None):
+        out = out if out else sys.stdout
+        average_grade = (student.math_grade + student.literature_grade + student.science_grade) / 3
+        print(f"Student: {student.student_name},", file=out, sep='\n')
+        print(f"Math Grade: {student.math_grade},", file=out, sep='\n')
+        print(f"Literature Grade: {student.literature_grade},", file=out, sep='\n')
+        print(f"Science Grade: {student.science_grade},", file=out, sep='\n')
+        print(f"Average Grade: {average_grade},", file=out, sep='\n')
+
+class Student:
     def __init__(self, student_name, math_grade, literature_grade, science_grade):
         self.student_name = student_name
         self.math_grade = math_grade
         self.literature_grade = literature_grade
         self.science_grade = science_grade
 
-    def generate_report(self, out: Optional[StringIO]=None):
-        out = out if out else sys.stdout
-        average_grade = (self.math_grade + self.literature_grade + self.science_grade) / 3
-        print(f"Student: {self.student_name},", file=out, sep='\n')
-        print(f"Math Grade: {self.math_grade},", file=out, sep='\n')
-        print(f"Literature Grade: {self.literature_grade},", file=out, sep='\n')
-        print(f"Science Grade: {self.science_grade},", file=out, sep='\n')
-        print(f"Average Grade: {average_grade},", file=out, sep='\n')
+
+
 
 if __name__ == "__main__":
-    student1 = StudentReport("John Doe", 90, 85, 95)
+    student1 = Student("John Doe", 90, 85, 95)
     student1.generate_report()
--- tests/test_studentreport.py
+++ tests/test_studentreport.py
@@ -1,15 +1,17 @@
 from io import StringIO
-from reporting.models import StudentReport
+from reporting.models import Report, Student
 
-def test_instantiation():
-    # __Given__
-    report = StudentReport("John Doe", 90, 85, 95)
+
+def test_report():
+    student = Student("John Doe", 90, 85, 95)
     out = StringIO()
+    report = Report()
     # __When__
-    report.generate_report(out)
+    report.generate_report(student, out)
     # __Then__
     assert out.getvalue() == 'Student: John Doe,\n' \
         'Math Grade: 90,\n' \
         'Literature Grade: 85,\n' \
         'Science Grade: 95,\n' \
         'Average Grade: 90.0,\n'
+    
